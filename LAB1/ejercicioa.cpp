721c115393ae577555a0f47eb9d4204a1fd4bdb0
Brigitte Bernal Belisario <131219987+Fx2048@users.noreply.github.com>
2025-03-27 09:14:19 -0500

    Create ejercicioa.cpp

---
+++ LAB1/ejercicioa.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+
+int main()
+{ 
+    // declaramos variables a utilizar
+    int num1, num2, sum, rest;
+    
+// solicitamos ingresar los numeros
+    printf("ingresa primer numero:");
+    scanf("%d", &num1);
+    
+    printf("ingresa el segundo numero:");
+    scanf("%d",&num2);
+    //realizamos operaciones de ejercicio
+    
+    sum = num1+num2;
+    rest= num1-num2;
+    
+    // mostramos resultados
+    printf( " La suma de  %d y %d es: %d  %d\n", num1, num2, sum );
+    printf( " La rest de  %d y %d es: %d  %d\n", num1, num2, rest );
+    return 0;
+    
+}
